c8a4e9a2c0700e26863cea7617a7295f7ccb7f42
Yonathan Soto <yotoelectronics@gmail.com>
2019-11-19 14:59:35 -0500

    one

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+import telebot
+
+# Token que entrega el botfather de telegram
+TOKEN = '123456:ABCDEFGHIJKLM'
+
+# Url de la aplicación heroku
+URL = 'https://nombre-de-la-aplicacion.herokuapp.com/'
+
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(func=lambda message: True, content_types=['new_chat_members'])
+def echo_message(message):
+	bot.send_message(message.chat.id, '<b>Bienvenide! '+message.new_chat_member.first_name + '</b>¿\n\nNos gustaria saber si eres programador y/o programador y de donde eres?',parse_mode='HTML')
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+# Se debe ingresar la primera vez a la url para setear el Webhook del bot
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL + TOKEN)
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
